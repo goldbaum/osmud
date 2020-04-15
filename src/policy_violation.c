28b33ee6dc1299303849bdf6ed1c184f90934df3
Ido Goldbaum <ido.goldbaum@guardicore.com>
2020-04-15 17:58:29 +0300

    report policy violations

--- src/policy_violation.c
+++ src/policy_violation.c
@@ -109,7 +109,9 @@ static void *pol_violation_thread_func(void *arg)
         else if (retval && FD_ISSET(syslog_fd, &rfds))
         {
             logOmsGeneralMessage(OMS_DEBUG, OMS_SUBSYS_POL_VIOLATION, "nfds read: %d", retval);
-            if (osm_read_line(syslog_line, MAX_SYSLOG_LINE, syslog_fd) > 1)
+            retval = osm_read_line(syslog_line, MAX_SYSLOG_LINE, syslog_fd);
+            logOmsGeneralMessage(OMS_DEBUG, OMS_SUBSYS_POL_VIOLATION, "readline len: %d", retval);
+            if (ret > 1)
             {
                 logOmsGeneralMessage(OMS_DEBUG, OMS_SUBSYS_POL_VIOLATION, "syslog line available");
                 logOmsGeneralMessage(OMS_DEBUG, OMS_SUBSYS_POL_VIOLATION, syslog_line);
